c3841f8390eb7980b891b607af5ee803be9a9591
alokartist <120403676+alokartist@users.noreply.github.com>
2023-01-10 22:26:44 +0530

    C program to print Fibonacci series program using recursive methods

---
+++ Fibonacci_r.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int fibonacci(int);
+int main()
+{ 
+	int n, i; 
+	printf("Enter the number of element you want in series :"); 
+	scanf("%d",&n); 
+	printf("fibonacci series is : ");
+	for(i=0;i<n;i++)
+    { 
+		printf("%d ",fibonacci(i));
+	}
+	getch();
+}
+
+int fibonacci(int i)
+{ 
+	if(i==0) return 0; 
+	else if(i==1) return 1; 
+	else return (fibonacci(i-1)+fibonacci(i-2));
+} 
